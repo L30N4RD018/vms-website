d069bef4ab42e4c65f5f20a22574c6299d6c4701
Carlos Guzmán <guzmanc@utb.edu.co>
2023-05-21 19:09:57 -0500

    vehicleseller

---
+++ src/pages/VehicleSeller.js
@@ -0,0 +1,22 @@
+import '../App.css';
+import Forml_seller from "../components/Forml_seller";
+import {Route, Routes} from "react-router-dom";
+
+
+import React from 'react'
+
+function VehicleSeller() {
+  return (
+
+    <section className='App'>
+    <section className='main_screen'>     
+      <section className='Forml_seller'>
+      <Forml_seller/>  
+      </section>
+      </section>
+  </section>
+   
+  )
+}
+
+export default VehicleSeller
